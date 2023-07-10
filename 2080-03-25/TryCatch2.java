bb1a34f3995a48404d1b20ceae35334976b8dfa3
Manan <135083037+NeupaneManan@users.noreply.github.com>
2023-07-10 17:29:34 +0545

    Add files via upload

---
+++ 2080-03-25/TryCatch2.java
@@ -0,0 +1,16 @@
+//Write a program to demonstrate exception and exception handeling.
+public class TryCatch2 {
+    public static void main(String[] args) {
+        try {
+            int result = 36 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        try {
+            int result = 36 / 0;
+        } finally {
+            int result = 36 / 6;
+            System.out.println("Finally block result:" + result);
+        }
+    }
+}
